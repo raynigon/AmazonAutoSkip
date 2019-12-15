98d55785ac7ac13852d169f650b37b10f63cc565
Simon Schneider <raynigon@users.noreply.github.com>
2019-12-15 23:30:16 +0100

    Automatically create Tags

--- create-release.sh
+++ create-release.sh
@@ -13,6 +13,13 @@ fi
 VERSION=`cat manifest.json | python3 -c "import sys, json; print(json.load(sys.stdin)['version'])"`
 COMMIT_LOG=`git log -1 --format='%s'`
 
+git tag | grep "v$VERSION"
+RESULT=$?
+if [ $RESULT -eq 1 ]; then
+    git tag -a "v$VERSION" -m "New Version with Improvements and Bugfixes. ${COMMIT_LOG}"
+fi
+git push --follow-tags
+
 web-ext build --ignore-files create-release.sh --overwrite-dest
 github-release upload \
   --owner "raynigon" \
